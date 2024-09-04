4d6d78f06349e435c2e59be57600202cc03aa230
DinaKSari <dinakumala7070@gmail.com>
2024-09-04 16:30:33 +0700

    Penambahan biodata Dina Kumala Sari ke branch davelop

---
+++ BiodataDina.java
@@ -0,0 +1,12 @@
+public class BiodataDina {
+    public static void main(String[] args) {
+        System.out.println("Nama     : DINA KUMALA SARI");
+        System.out.println("NIM      : 244107020072");
+        System.out.println("Kelas    : 1A");
+        System.out.println("No Absen : 11");
+        System.out.println("Prodi    : D-IV Teknik Informatika");
+        System.out.println("Alamat   : Jl.Mondoroko 1 no.30");
+        System.out.println("No HP    : 082372010600");
+        System.out.println("Hobi     : Menyanyi");
+    }
+}
