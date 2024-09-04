3cfd70b1290f928bdc33a626e2b2164d2761fc0b
sileysa <sileysafaedatulnuraini984@gmail.com>
2024-09-04 16:35:23 +0700

    Penambahan biodata Sileysa Faedatul Nuraini ke branch davelop-leli

---
+++ BiodataLeli.java
@@ -0,0 +1,13 @@
+public class BiodataLeli {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Sileysa Faedatul Nuraini");
+        System.out.println("NIM     : 244107020231");
+        System.out.println("Kelas   : 1A");
+        System.out.println("No Absen: 27");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Probolinggo");
+        System.out.println("No HP   : 082132847935");
+        System.out.println("Hobi    : Membaca");
+    }
+    
+}
